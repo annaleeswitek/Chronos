b448fbb1767ba14688fd99f6976f239a10b0b194
Lisa Shepard <lisashepard04@gmail.com>
2018-04-17 16:02:56 -0400

    remove extraneous stuff

--- client/components/AllProductsContainer.jsx
+++ client/components/AllProductsContainer.jsx
@@ -1,17 +1,17 @@
 import { connect } from 'react-redux';
 import AllProducts from './AllProducts';
 
+const mapStateToProps = function(state){
+    return {
+        products: state.products
+    }
+}
+
 const mapDispatchToProps = function(dispatch){
     return {
         getAllProducts: function(){
             dispatch(getAllProducts());
         }
-    }
-}
-
-const mapStateToProps = function(state){
-    return {
-        products: state.products
     }
 }
 
