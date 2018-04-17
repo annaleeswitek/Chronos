a63983cb08ea573f218a0f0aa99402951fc07dfe
GSSchneider <35821280+GSSchneider@users.noreply.github.com>
2018-04-17 15:39:23 -0400

    Create backend GET route for single product (by productId)

--- server/api/products.js
+++ server/api/products.js
@@ -1,11 +1,18 @@
 const router = require('express').Router();
 const {Product} = require('../db/models');
-module.exports = router
+
+module.exports = router;
 
 router.get('/', (req, res, next) => {
     Product.findAll({
         include: [{ all: true }]
     })
-    .then(products => res.json(products))
+    .then( products => res.json(products) )
     .catch(next);
-})
+});
+
+router.get('/:productId', (req, res, next) => {
+  Product.findById(req.params.productId)
+    .then( product => res.json(product) )
+    .catch(next);
+});
