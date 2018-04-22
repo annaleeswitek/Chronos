2c62904f8dc8c9664baeb5342b17e4995bf0688a
Abigail Demsas <ademsas1026@gmail.com>
2018-04-22 10:00:06 -0400

    added to cart, send back cart

--- client/components/SingleProduct.jsx
+++ client/components/SingleProduct.jsx
@@ -1,21 +1,38 @@
 'use strict';
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
-import { Button } from 'react-bootstrap';
+import { Button, FormGroup, FormControl } from 'react-bootstrap';
 
 import { EditProduct } from './index';
 import { fetchOneProduct, addToCart } from '../store';
 
 /* ---- Component ---- */
 class SingleProduct extends Component {
+  constructor(){
+    super();
+    this.state = {
+      quantity: 0, 
+      disabled: true
+    };
+    this.onQuantityChange = this.onQuantityChange.bind(this);
+  }
 
   componentDidMount(){
     this.props.fetchOneProduct(this.props.match.params.productId);
   }
 
-  render(){
-  const { user, product, addToCart } = this.props;
+  onQuantityChange(event){
+    console.log('event in on quant change: ', event.target.value); 
+    if(event.target.value) this.setState({ quantity: event.target.value, disabled: false })
+    else this.setState({disabled: true })
+    
+  }
 
+  render(){
+    const { user, product, addToCart } = this.props;
+    const { quantity, disabled } = this.state;
+    const options = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+    console.log('state quantity: ', quantity);
     return (
       <div className="product">
         { product.id
@@ -23,12 +40,19 @@ class SingleProduct extends Component {
               <div id="allButDesc">
                 <h3>{ product.title }</h3>
                 <img src={ product.imgUrl } />
-                <h4>${ product.price }</h4>
-                <Button onClick={addToCart.bind(this, product)}>Add To Cart</Button>
+                
+                
               </div>
               <div id="description">
                 <h4>{ product.description }</h4>
-
+                <h4>${ product.price }</h4>
+                <FormGroup>
+                  <FormControl type="text" value={quantity} name="quantity" componentClass="select" onChange={this.onQuantityChange}>
+                   { options.map(option => <option key={option} value={option}>{option}</option>)}
+                  </FormControl>
+                  <Button onClick={addToCart.bind(this, product, quantity)} disabled={disabled}>Add To Cart</Button>
+                </FormGroup>
+                
               </div>
             </section>
           : <h2>Product Not Found</h2>
@@ -47,15 +71,17 @@ class SingleProduct extends Component {
 /* ---- Container ---- */
 const mapState = state => ({
   product: state.product,
-  user: state.user
+  user: state.user, 
+  cart: state.cart
 });
 
 const mapDispatch = dispatch => ({
   fetchOneProduct(productId) {
     dispatch(fetchOneProduct(productId));
   }, 
-  addToCart(product, event){
-    // console.log('product ', product);
+  addToCart(product, quantity, event){
+    event.preventDefault();
+    product.quantity = quantity;
     dispatch(addToCart(product));
   }
 });
