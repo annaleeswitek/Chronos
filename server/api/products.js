479940bb4b8332169864d1ff1bc4e9ffc46d9b40
Abigail Demsas <ademsas1026@gmail.com>
2018-04-25 10:57:39 -0400

    oops

--- server/api/products.js
+++ server/api/products.js
@@ -1,7 +1,7 @@
 'use strict';
 
 const router = require('express').Router();
-const { Product, Category } = require('../db/models');
+const { Product, Category, ProductCategory } = require('../db/models');
 
 module.exports = router;
 
@@ -19,40 +19,27 @@ router.get('/:productId', (req, res, next) => {
     .catch(next);
 });
 
-//adding new product - admin
-// router.post('/', (req, res, next) => {
-//     Product.create(req.body)
-//     .then(newProduct => {
-//         res.json(newProduct);
-//     })
-//     .catch(next);
-// });
 
 router.post('/', async (req, res, next) => {
-    console.log('req.body.categories', req.body.categories);
     const categoryNames = req.body.categories.split(', ');
-    console.log('categoryNames', categoryNames)
-    const productCategoryPromises = categoryNames.map(categoryName => {
-        return Category.findOrCreate({where: { name: categoryName }})
-    })
-    console.log('product category promises in back end: ', productCategoryPromises)
-    let categoriesArrays = await Promise.all(productCategoryPromises).catch(next);
-    console.log('categories arrays: ', categoriesArrays);
-    let productCategoriesArray = categoriesArrays.map(categoryArray => categoryArray);
-    console.log('produce categories array: ', productCategoriesArray);
-    let productCategories = productCategoriesArray.map(productCategory => productCategory[0].dataValues);
-    console.log('product categories: ', productCategories);
-    let newProduct = await Product.create({
-        title: req.body.title, 
-        price: req.body.price, 
-        description: req.body.description, 
-        quantity: req.body.quantity, 
-        imgUrl: req.body.imgUrl, 
-        categories: productCategories
-    })
-    .catch(next);
+    //create new product
+    let newProduct = await Product.create(req.body).catch(next);
+    //create or find categor(y)(ies)  
+    const categoryPromiseArrays = categoryNames.map(async categoryName => await Category.findOrCreate({ 
+        where: { name: categoryName }
+    }).catch(next));
+    //the following returns an array of arrays [[category, bool], [category, bool]...[category, bool]]
+    const categoryArrays = await Promise.all(categoryPromiseArrays);
+    const categories = categoryArrays.map(categoryArray => categoryArray[0]);
+    //create ProductCategory instance
+    if (categories) categories.map(async category => 
+        await ProductCategory.create({
+             productId: newProduct.id, 
+             categoryId: category.id 
+            })
+            .catch(next));
     res.json(newProduct);
-})
+});
 
 // editing product - admin
 router.put('/:productId', (req, res, next) => {
