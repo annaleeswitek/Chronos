f340ff0a86f0d6a5eb6409721c7782d185106693
Abigail Demsas <ademsas1026@gmail.com>
2018-07-24 17:18:41 -0400

    landing page functional

--- client/App.jsx
+++ client/App.jsx
@@ -1,14 +1,63 @@
-import React from 'react';
-import { Navbar } from './components';
+import React, { Component } from 'react';
+import { connect } from 'react-redux'
+import { withRouter, Route, Switch } from 'react-router-dom'
+
+import { Navbar, Homepage } from './components';
 import Routes from './Routes.jsx';
+import { me } from './store'
 
-const App = () => {
-  return (
-    <div>
-      <Navbar />
-      <Routes />
-    </div>
-  );
-};
+class App extends Component {
+  constructor(){
+    super()
+    this.state = {
+      clickedEnter: false
+    }
+    this.enterSite = this.enterSite.bind(this)
+  }
 
-export default App;
+  componentDidMount () {
+    this.props.loadInitialData();
+  }
+
+  enterSite(){
+    this.setState(prevState => {
+      return { clickedEnter: !prevState.clickedEnter }
+    })
+  }
+
+  render(){
+    const { clickedEnter } = this.state
+    console.log('i have clicked enter: ', clickedEnter)
+    return (
+      <div>
+      { !clickedEnter && <Homepage enterSite={this.enterSite}/>}
+      { clickedEnter &&
+        <div>
+        <Navbar />
+        <Routes />
+        </div>
+      }
+        
+      </div>
+    );
+  }
+}
+  
+
+
+
+
+const mapState = state => ({
+  // Being 'logged in' for our purposes will be defined has having a state.user that has a truthy id.
+  // Otherwise, state.user will be an empty object, and state.user.id will be falsey
+  isLoggedIn: !!state.user.id,
+  user: state.user
+});
+
+const mapDispatch = dispatch => ({
+loadInitialData () {
+  dispatch(me());
+}
+});
+
+export default withRouter(connect(mapState, mapDispatch)(App));
--- client/components/Homepage.jsx
+++ client/components/Homepage.jsx
@@ -1,34 +1,15 @@
 'use strict';
 
-import React, { Component } from 'react'
-import { Button } from '@material-ui/core'
+import React from 'react'
 
 /* ---- Component ---- */
-export default class Homepage extends Component {
-  constructor(){
-    super()
-    this.state = {
-      showLogin: false
-    }
-    this.showLogin = this.showLogin.bind(this)
-  }
+const Homepage = props => (
+  <div id="home">
+    <button onClick={props.enterSite} id="homeButton">
+      <h1 id="homeTitle">Chronos</h1>
+      <h3 id="homeCaption">Enter Site</h3>
+    </button>
+  </div>
+)
 
-  showLogin(){
-    this.setState(prevState => {
-      return { showLogin: !prevState.showLogin }
-    })
-  }
-  render(){
-    return (
-      <div id="home">
-        <Button onClick={this.showLogin}>
-          <h1 id="homeTitle">Chronos</h1>
-        </Button>
-      </div>
-    )
-  }
-}
-
-
-
-
+export default Homepage
